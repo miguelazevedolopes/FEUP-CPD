8b4981fc1a2bf30756ffb28552ebf1de34acd8c1
up201907284 <up201907284@fe.up.pt>
2022-05-15 15:49:54 +0100

    documented the interfaces from point 2

--- assign2/src/com/cpd2/main/service/ClusterMembership.java
+++ assign2/src/com/cpd2/main/service/ClusterMembership.java
@@ -1,8 +1,19 @@
 package com.cpd2.main.service;
 
+
+/**
+ * This interface allows the user to add/remove nodes to the cluster.
+ */
 public interface ClusterMembership {
 
+    /**
+     * Adds a node to the cluster
+     */
     void join();
 
+
+    /**
+     * Removes a node from the cluster
+     */
     void leave();
 }
--- assign2/src/com/cpd2/main/service/KeyValueStore.java
+++ assign2/src/com/cpd2/main/service/KeyValueStore.java
@@ -1,11 +1,32 @@
 package com.cpd2.main.service;
 
+/**
+ * This interface represents the persistent key-value Store.
+ * @param <K> any class K key.
+ * @param <V> any class V value.
+ */
 public interface KeyValueStore<K, V>{
 
+    /**
+     * Adds a key-value pair to the store.
+     * @param key class K key determined by the interface implementation.
+     * @param value class V value determined by the interface implementation.
+     */
     void put(K key, V value);
 
+
+    /**
+     * Retrieves the value bound to a key
+     * @param key class K key determined by the interface implementation.
+     * @return class V value determined by the interface implementation.
+     */
     V get(K key);
 
+
+    /**
+     * Deletes a key-value pair from the store.
+     * @param key class K key determined by the interface implementation.
+     */
     void delete(K key);
 
 }
