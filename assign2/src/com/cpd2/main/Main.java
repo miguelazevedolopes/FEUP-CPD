f148fcbd9c74b5be2078ef0bd62513b9ba582586
Vasco Marinho Rodrigues Gomes Alves <up201808031@up.pt>
2022-05-18 10:07:13 +0100

    add hashing function

--- assign2/src/com/cpd2/main/Main.java
+++ assign2/src/com/cpd2/main/Main.java
@@ -1,14 +1,15 @@
 package com.cpd2.main;
 
+import java.security.NoSuchAlgorithmException;
+
 import com.cpd2.main.service.MembershipView;
-import com.cpd2.main.service.Node;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
         System.out.println("Hello world!");
-        MembershipView mv1=new MembershipView(11,3);
-        MembershipView mv2=new MembershipView("/home/miguel/Documents/Faculdade/g01/assign2/storage/11/membership");
-        System.out.println(mv2);
+        MembershipView mv1=new MembershipView(12,3);
+        //MembershipView mv2=new MembershipView("/home/miguel/Documents/Faculdade/g01/assign2/storage/11/membership");
+        //System.out.println(mv2);
     }
 }
