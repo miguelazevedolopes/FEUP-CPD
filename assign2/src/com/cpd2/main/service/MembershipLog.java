a3b083c34a030a9fadb20619d5ec0a0fbabcc84a
up201907284 <up201907284@fe.up.pt>
2022-05-20 22:02:16 +0100

    log updater updated to cover different scenarios

--- assign2/src/com/cpd2/main/service/MembershipLog.java
+++ assign2/src/com/cpd2/main/service/MembershipLog.java
@@ -9,8 +9,30 @@ public class MembershipLog implements Serializable{
     MembershipLog(Integer nodeID, Integer membershipCount){
         membershipLog.put(nodeID, membershipCount);
     }
-    
+
+
+    /**
+     * Updates the log table, maintaining consistency in its logs
+     * @param nodeID the node to be updated
+     * @param membershipCount the new count to be set
+     */
     void updateNodeView(Integer nodeID, Integer membershipCount){
-        membershipLog.put(nodeID, membershipCount);
+
+        if(membershipLog.containsKey(nodeID))
+        {
+            //Event is already in the local log or if the event is older than the event for that member in the local log
+            if(membershipLog.get(nodeID) == membershipCount || membershipLog.get(nodeID) > membershipCount)
+                return;
+            //Event is newer than the event for that member in the local log
+            else
+            {
+                membershipLog.remove(nodeID, membershipLog.get(nodeID));
+                membershipLog.put(nodeID, membershipCount);
+            }
+        }
+        else //New event
+        {
+            membershipLog.put(nodeID, membershipCount);
+        }
     }
 }
