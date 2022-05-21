90dba8f49bcf94071f076c48b49da3c873e5fb25
up201907284 <up201907284@fe.up.pt>
2022-05-21 18:11:17 +0100

    added a custom Pair class and changed membershipLog class

--- assign2/src/com/cpd2/main/service/MembershipLog.java
+++ assign2/src/com/cpd2/main/service/MembershipLog.java
@@ -1,13 +1,19 @@
 package com.cpd2.main.service;
+
+import com.cpd2.main.Pair;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MembershipLog implements Serializable{
-    private Map<Integer,Integer> membershipLog=new HashMap<>();
+    //private Map<Integer,Integer> membershipLog=new HashMap<>();
+    private List<Pair<Integer, Integer>> memLog = new ArrayList<>();
 
     MembershipLog(Integer nodeID, Integer membershipCount){
-        membershipLog.put(nodeID, membershipCount);
+        //membershipLog.put(nodeID, membershipCount);
+        memLog.add(new Pair<>(nodeID, membershipCount));
     }
 
 
@@ -18,21 +24,33 @@ public class MembershipLog implements Serializable{
      */
     void updateNodeView(Integer nodeID, Integer membershipCount){
 
-        if(membershipLog.containsKey(nodeID))
+        Pair<Integer, Integer> toBeAdded = new Pair<>(nodeID, membershipCount);
+
+        for(int i = 0; i < memLog.size(); i++)
         {
-            //Event is already in the local log or if the event is older than the event for that member in the local log
-            if(membershipLog.get(nodeID) == membershipCount || membershipLog.get(nodeID) > membershipCount)
+            //Event is already in the local log
+            if(memLog.get(i).isEqual(toBeAdded))
+                return;
+            //The event is older than the event for that member in the local log
+            else if(memLog.get(i).getL() == toBeAdded.getL() && memLog.get(i).getR() > membershipCount)
                 return;
             //Event is newer than the event for that member in the local log
-            else
+            else if(memLog.get(i).getL() == toBeAdded.getL() && memLog.get(i).getR() < membershipCount)
             {
-                membershipLog.remove(nodeID, membershipLog.get(nodeID));
-                membershipLog.put(nodeID, membershipCount);
+                memLog.remove(memLog.get(i));
+                memLog.add(toBeAdded);
+                return;
             }
         }
-        else //New event
+        //New event
+        memLog.add(toBeAdded);
+    }
+
+    void checkUpdated(List<Pair<Integer, Integer>> toBeCompared)
+    {
+        for(int i = 0; i < toBeCompared.size(); i++)
         {
-            membershipLog.put(nodeID, membershipCount);
+            updateNodeView(toBeCompared.get(i).getL(), toBeCompared.get(i).getR());
         }
     }
 }
